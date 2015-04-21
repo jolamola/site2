d2d0f6fde2066a53641ba892021f380698567c83
jolamola <mrdanielkim@gmail.com>
2015-04-20 18:40:56 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+	"use strict"; 
+
+	var wheight = $(window).height();
+
+	//replace height with window height
+	$('.fullheight').css('height',wheight);
+
+	//adjust height of ful height elements when window resize
+	$(window).resize(function(){
+		wheight = $(window).height();
+		$('fullheight').css('height', wheight);
+	});
+})
