935b545eb797d63395bbc90dedd0704df3275436
jolamola <mrdanielkim@gmail.com>
2015-04-29 17:56:42 -0700

    rwd attempt1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-$(function(){
-	"use strict"; 
+// $(function(){
+// 	"use strict"; 
 
-	var wheight = $(window).height();
+// 	var wheight = $(window).height();
 
-	//replace height with window height
-	$('.fullheight').css('height',wheight);
+// 	//replace height with window height
+// 	$('.fullheight').css('height',wheight);
 
-	//adjust height of ful height elements when window resize
-	$(window).resize(function(){
-		wheight = $(window).height();
-		$('fullheight').css('height', wheight);
-	});
-})
+// 	//adjust height of ful height elements when window resize
+// 	$(window).resize(function(){
+// 		wheight = $(window).height();
+// 		$('fullheight').css('height', wheight);
+// 	});
+// })
+
