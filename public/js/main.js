34bb7f6826f4a73602641f10b05ea7c3a8b33394
jolamola <mrdanielkim@gmail.com>
2015-05-01 13:23:01 -0700

    ooga booga2

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,10 @@
 	// Wait for window load
 	$(window).load(function() {
 		// Animate loader off screen
-		$(".loader").fadeOut("fast");;
-		$("body").addClass(".intro")
+		$(".loader").fadeOut("fast");
+
 	});
 
-	
+	$(document).ready(function(){
+				$("body").addClass(".intro");
+	});
