f1c0fb9f4c3d4c7e5408195b01937a1de0a6afbb
jolamola <mrdanielkim@gmail.com>
2015-05-01 12:09:07 -0700

    load screen

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,4 @@
-// $(function(){
-// 	"use strict"; 
 
-// 	var wheight = $(window).height();
-
-// 	//replace height with window height
-// 	$('.fullheight').css('height',wheight);
-
-// 	//adjust height of ful height elements when window resize
-// 	$(window).resize(function(){
-// 		wheight = $(window).height();
-// 		$('fullheight').css('height', wheight);
-// 	});
-// })
-
-//paste this code under head tag or in a seperate js file.
 	// Wait for window load
 	$(window).load(function() {
 		// Animate loader off screen
